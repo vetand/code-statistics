624f6b22539abc18634aeaca3aa2cee882478518
Nolan16 <66615481+Nolan16@users.noreply.github.com>
2021-04-05 11:52:03 +0300

    Update ProjectReport.java

--- CodeStats/src/main/java/codestats/ProjectReport.java
+++ CodeStats/src/main/java/codestats/ProjectReport.java
@@ -1,19 +1,32 @@
 package codestats;
 
-public class ProjectReport extends Report {
+import java.util.HashMap;
+
+public class ProjectReport {
+
+  HashMap<String, Report> fileStats_;
+
   public ProjectReport() {
-    super();
+    fileStats_ = new HashMap<String, Report>();
   }
 
-  public void addFileReport(Report fileReport) {
-    fileReport.getStats().entrySet().forEach(entry -> {
-      if (stats_.containsKey(entry.getKey())) {
-        stats_.put(entry.getKey(), Integer.toString(
-            Integer.parseInt(stats_.get(entry.getKey()))
-                + Integer.parseInt(entry.getValue())));
-      } else { // values should be ints in string format!
-        stats_.put(entry.getKey(), entry.getValue());
-      }
-    });
+  public HashMap<String, Report> getFileReports() {
+    return fileStats_;
+  }
+
+  public void addFileReport(String fileName, Report fileReport) {
+    if (fileStats_.containsKey(fileName)) {
+      fileReport.getStats().entrySet().forEach(entry -> {
+        if (fileStats_.get(fileName).getStats().containsKey(entry.getKey())) {
+          fileStats_.get(fileName).getStats().put(entry.getKey(), Integer.toString(
+                  Integer.parseInt(fileStats_.get(fileName).getStats().get(entry.getKey()))
+                          + Integer.parseInt(entry.getValue())));
+        } else { // values should be ints in string format!
+          fileStats_.get(fileName).getStats().put(entry.getKey(), entry.getValue());
+        }
+      });
+    } else {
+      fileStats_.put(fileName, fileReport);
+    }
   }
 }
