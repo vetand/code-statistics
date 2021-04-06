1c705f8768a07e365b2133984792aed44a35b042
Nolan16 <66615481+Nolan16@users.noreply.github.com>
2021-04-07 00:34:00 +0300

    Update ProjectReport.java

--- CodeStats/src/main/java/codestats/ProjectReport.java
+++ CodeStats/src/main/java/codestats/ProjectReport.java
@@ -2,31 +2,45 @@ package codestats;
 
 import java.util.HashMap;
 
-public class ProjectReport {
+public class ProjectReport extends Report {
 
-  HashMap<String, Report> fileStats_;
+  private HashMap<String, Report> fileStats;
 
   public ProjectReport() {
-    fileStats_ = new HashMap<String, Report>();
+    super();
+    fileStats = new HashMap<String, Report>();
   }
 
   public HashMap<String, Report> getFileReports() {
-    return fileStats_;
+    return fileStats;
+  }
+
+
+  public void sumFileReport(Report fileReport) {
+    fileReport.getStats().entrySet().forEach(entry -> {
+      if (stats_.containsKey(entry.getKey())) {
+        stats_.put(entry.getKey(), Integer.toString(
+                Integer.parseInt(stats_.get(entry.getKey()))
+                        + Integer.parseInt(entry.getValue())));
+      } else { // values should be ints in string format!
+        stats_.put(entry.getKey(), entry.getValue());
+      }
+    });
   }
 
   public void addFileReport(String fileName, Report fileReport) {
-    if (fileStats_.containsKey(fileName)) {
+    if (fileStats.containsKey(fileName)) {
       fileReport.getStats().entrySet().forEach(entry -> {
-        if (fileStats_.get(fileName).getStats().containsKey(entry.getKey())) {
-          fileStats_.get(fileName).getStats().put(entry.getKey(), Integer.toString(
-                  Integer.parseInt(fileStats_.get(fileName).getStats().get(entry.getKey()))
+        if (fileStats.get(fileName).getStats().containsKey(entry.getKey())) {
+          fileStats.get(fileName).getStats().put(entry.getKey(), Integer.toString(
+                  Integer.parseInt(fileStats.get(fileName).getStats().get(entry.getKey()))
                           + Integer.parseInt(entry.getValue())));
         } else { // values should be ints in string format!
-          fileStats_.get(fileName).getStats().put(entry.getKey(), entry.getValue());
+          fileStats.get(fileName).getStats().put(entry.getKey(), entry.getValue());
         }
       });
     } else {
-      fileStats_.put(fileName, fileReport);
+      fileStats.put(fileName, fileReport);
     }
   }
 }
