3119165b68d445797dea8b7e115b411759cf07d8
Konstantin Matveev <uragankm@mail.ru>
2021-04-08 18:30:01 +0300

    implement comment lines stats collector

--- CodeStats/src/test/java/codestats/UnitTests.java
+++ CodeStats/src/test/java/codestats/UnitTests.java
@@ -2,6 +2,8 @@ package codestats;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
+
+import java.io.File;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
@@ -13,19 +15,29 @@ public class UnitTests {
     // мб имя файла неправильное, можно поменять
     Report report = statMaker.collect("src/main/resources/Root/code-example.cpp");
     HashMap<String, String> result = report.getStats();
-    assertEquals(result.size(), 3);
-    assertEquals(result.get("Total comment lines"), "3");
-    assertEquals(result.get("Total empty comment lines"), "1");
-    assertEquals(result.get("Total filled comment lines"), "2");
+    assertEquals(result.size(), 4);
+    assertEquals(result.get("Total lines with comments"), "3");
+    assertEquals(result.get("Total single-line comments"), "3");
+    assertEquals(result.get("Empty single-line comments"), "1");
+    assertEquals(result.get("Total multi-line comments"), "0");
+
+    report = statMaker.collect("src/main/resources/Root/code-example-with-scope-comments.cpp");
+    result = report.getStats();
+    assertEquals(result.size(), 4);
+    assertEquals(result.get("Total lines with comments"), "16");
+    assertEquals(result.get("Total single-line comments"), "5");
+    assertEquals(result.get("Empty single-line comments"), "0");
+    assertEquals(result.get("Total multi-line comments"), "8");
   }
   @Test
   public void testProjectTreeFilesDIR() {
     ProjectTree tree = new ProjectTree("src/main/resources/Root");
     List<String> files = tree.getValidFiles();
     Iterator<String> iter = files.iterator();
-    assertEquals(files.size(), 2);
-    assertEquals(iter.next(), "src/main/resources/Root/code-example.cpp");
-    assertEquals(iter.next(), "src/main/resources/Root/Subdir/another-example.java");
+    assertEquals(files.size(), 3);
+    assertEquals(iter.next(), new File("src/main/resources/Root/code-example-with-scope-comments.cpp").getPath());
+    assertEquals(iter.next(), new File("src/main/resources/Root/code-example.cpp").getPath());
+    assertEquals(iter.next(), new File("src/main/resources/Root/Subdir/another-example.java").getPath());
   }
 
   @Test
@@ -33,6 +45,7 @@ public class UnitTests {
     ProjectTree tree = new ProjectTree("src/main/resources/Root");
     String structure = tree.getProjectTreeReport();
     assertEquals(structure, "Root/\n"
+                                + "├── code-example-with-scope-comments.cpp\n"
                                 + "├── code-example.cpp\n"
                                 + "└── Subdir/\n"
                                 + "    └── another-example.java\n");
