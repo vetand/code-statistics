260a5bce9b02bf97f193b572494bd5758bb06ad3
Nolan16 <66615481+Nolan16@users.noreply.github.com>
2021-04-18 23:54:22 +0300

    Update Report.java

--- CodeStats/src/main/java/codestats/Report.java
+++ CodeStats/src/main/java/codestats/Report.java
@@ -1,19 +1,47 @@
 package codestats;
 
+import java.util.Map;
 import java.util.HashMap;
+import java.util.SortedSet;
+import java.util.TreeSet;
+import java.util.Comparator;
 
 public abstract class Report {
-  HashMap<String, String> stats_;
+  HashMap<String, String> stats;
 
   public Report() {
-    stats_ = new HashMap<String, String>();
+    stats = new HashMap<String, String>();
+  }
+
+  static private SortedSet<Map.Entry<String,String>> SortByValues(Map<String,String> map) {
+    SortedSet<Map.Entry<String,String>> sorted = new TreeSet<Map.Entry<String,String>>(
+            new Comparator<Map.Entry<String,String>>() {
+              @Override public int compare(Map.Entry<String,String> s1, Map.Entry<String,String> s2) {
+                int res = s1.getValue().compareTo(s2.getValue());
+                if (s1.getKey().equals(s2.getKey())) {
+                  return res;
+                } else {
+                  return res == 0 ? 1 : res;
+                }
+              }
+            });
+    sorted.addAll(map.entrySet());
+    return sorted;
   }
 
   public void addStat(String key, String value) {
-    stats_.put(key, value);
+    stats.put(key, value);
   }
 
   public HashMap<String, String> getStats() {
-    return stats_;
+    return stats;
+  }
+
+  public SortedSet<Map.Entry<String,String>> getSortedStats() {
+    return SortByValues(stats);
+  }
+
+  public int size() {
+    return stats.size();
   }
 }
