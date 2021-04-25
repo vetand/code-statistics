c8d324d65bd775f321529a55a90e7ec484d25036
Nolan16 <66615481+Nolan16@users.noreply.github.com>
2021-04-25 15:52:44 +0300

    Update TextLayout.java

--- CodeStats/src/main/java/codestats/TextLayout.java
+++ CodeStats/src/main/java/codestats/TextLayout.java
@@ -1,6 +1,8 @@
 package codestats;
 
 import java.util.Map;
+import java.util.List;
+import java.util.Arrays;
 
 public class TextLayout implements Layout {
   public String toString(ProjectReport report,
@@ -12,21 +14,40 @@ public class TextLayout implements Layout {
       res += "Project is empty.\n";
       return res;
     }
+    List<String> groups = Arrays.asList("comments", "occurrences", "Declarations");
     if (showTree) {
       res += tree.getProjectTreeReport() + "\n\n";
     }
 
     if (mode.base()) {
       res += "\t\t\tWHOLE PROJECT REPORT:\n\n";
-      for (Map.Entry<String, String> stat : report.getSortedStats()) {
-        res += (stat.getKey() + ": " + stat.getValue() + "\n");
+      for (String group : groups) {
+        boolean wasGroup = false;
+        for (Map.Entry<String, String> stat : report.getSortedStats()) {
+          if (stat.getKey().contains(group)) {
+            res += (stat.getKey() + ": " + stat.getValue() + "\n");
+            wasGroup = true;
+          }
+        }
+        if (wasGroup) {
+          res += "\n";
+        }
       }
     } else {
       res += "\t\t\tREPORT FOR EACH FILE:\n\n";
       for (Map.Entry<String, Report> file : report.getFileReports().entrySet()) {
         res += (file.getKey() + ":\n");
-        for (Map.Entry<String, String> stat : file.getValue().getSortedStats()) {
-          res += ("\t" + stat.getKey() + ": " + stat.getValue() + "\n");
+        for (String group : groups) {
+          boolean wasGroup = false;
+          for (Map.Entry<String, String> stat : file.getValue().getSortedStats()) {
+            if (stat.getKey().contains(group)) {
+              res += ("\t" + stat.getKey() + ": " + stat.getValue() + "\n");
+              wasGroup = true;
+            }
+          }
+          if (wasGroup) {
+            res += "\n";
+          }
         }
         res += "\n";
       }
